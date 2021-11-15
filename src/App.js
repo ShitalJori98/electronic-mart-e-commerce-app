0ca7feaff52d391c26d43a30a01c57725746d293
ShitalJori98 <shital.jori@clairvoyantsoft.com>
2021-11-15 18:07:03 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+       <Home/>
+       <Navbar/>
     </div>
   );
 }
